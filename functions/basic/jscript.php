39cdb9cb14e0b0cb96b4ca4041ae8b00f423847b
DSchuppelius <daniel.schuppelius@gmail.com>
2025-09-28 10:00:12 +0200

    fix style

--- functions/basic/jscript.php
+++ functions/basic/jscript.php
@@ -51,7 +51,7 @@ function show_scriptconsent() {
     // "Ablehnen"-Button
     echo '<button class="btn no" type="button" onclick="document.cookie=\'scriptconsent_estatus=dismiss;path=/;SameSite=Lax' . (is_ssl() ? ';Secure' : '') . '\'; document.getElementById(\'script_fullscreen\').classList.remove(\'open\');">';
     echo esc_html($current_setup->get("noscript_link"));
-    echo '</button>';
+    echo '</button><br />';
     echo '<button class="btn all-ok" onclick="document.cookie=\'cookieconsent_estatus=allow;path=/;SameSite=Lax' . (is_ssl() ? ';Secure' : '') . '\'; document.cookie=\'scriptconsent_estatus=dismiss;path=/;SameSite=Lax' . (is_ssl() ? ';Secure' : '') . '\'; location.reload(true);" type="button">' . mb_encode_numericentity($current_setup->get("script_cookie_link"), [0x80, 0x10FFFF, 0, 0x10FFFF], "UTF-8") . '</button>';
     // Formular schließen
     echo '</form>';
